d423782b41b0f2d73ef90daf685684515bcd7af0
unknown <evan.doict@gmail.com>
2024-10-21 17:32:10 +0600

    servr product

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const port =process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//bootcamp_db_user
-//ZAQ!2wsx
-
-
 
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_SECRET}@cluster0.1kugq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -35,7 +31,7 @@ async function run() {
    // const userCollection = client.db("usersDbBootcamp").collection("users");
     const categoryCollection = client.db("ProductDB").collection("Category");
 
-//add Category
+// Category
 app.get("/getcategory", async (req, res) => {
     const query = categoryCollection.find();
     const result = await query.toArray();
@@ -87,6 +83,63 @@ app.get("/getcategory", async (req, res) => {
       );
       res.send(result);
     });
+
+    // Product query
+
+    const productCollection = client.db("ProductDB").collection("Products");  
+
+app.get("/getproduct", async (req, res) => {
+  const query = productCollection.find();
+  const result = await query.toArray();
+  res.send(result);
+}); 
+
+app.post("/addproduct", async (req, res) => {
+  const product = req.body;
+  console.log(product);
+  const result = await productCollection.insertOne(product);
+  res.send(result);
+});
+
+app.get("/productadmin/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const query = { _id: new ObjectId(id) };
+  const result = await productCollection.findOne(query);
+  console.log(result);
+  res.send(result);
+});
+
+app.delete("/productdelete/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const query = { _id: new ObjectId(id) };
+  const result = await productCollection.deleteOne(query); 
+  res.send(result); 
+});
+
+app.put("/productedit/:id", async (req, res) => { 
+  const id = req.params.id;
+  const user = req.body;
+  console.log(id, user);
+
+  const filter = { _id: new ObjectId(id) };
+  const option = { upsert: true };
+
+  const updatedUser = {
+    $set: {
+      name: user.name,
+      photo: user.photo, 
+      category:user.category,
+    },
+  };
+  const result = await productCollection.updateOne(
+      filter,
+      updatedUser,
+      option
+    );
+    res.send(result);
+  });
 
 
 
