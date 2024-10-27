eff7c450f9d54735749b0ea9f7844d8382fa4deb
unknown <evan.doict@gmail.com>
2024-10-27 23:56:32 +0600

    ok

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.use(cors())
 app.use(express.json());
 
 
+
+
 //const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_SECRET}@cluster0.1kugq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 //const uri = `mongodb+srv://bootcamp_db_user:ZAQ!2wsx@cluster0.1kugq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
